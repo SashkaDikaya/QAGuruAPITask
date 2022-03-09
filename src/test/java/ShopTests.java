7eab4b80537454a09364a668194c3243e4714326
SashkaDikaya <dikayaav@gmail.com>
2022-03-09 21:20:27 +0300

    add loginTest and addCart test and addWishLisTest

--- src/test/java/ShopTests.java
+++ src/test/java/ShopTests.java
@@ -1,3 +1,7 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
@@ -5,12 +9,29 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.closeWindow;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 
 public class ShopTests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+    }
+
+    @BeforeEach
+    void beforeEach () {
+        open(baseURI);
+    }
+
+    @AfterEach
+    void afterEch() {
+        closeWindow();
+    }
 
     @Test
     void addItemIntoCardTest() {
@@ -18,11 +39,12 @@ public class ShopTests {
         Integer cardSize = 1;
 
         given()
+                .log().all()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .cookie("Nop.customer=5dde7bcb-dbd6-4064-b0e7-18739a4fed42")
                 .body("product_attribute_28_7_10=28&product_attribute_28_1_11=31&addtocart_28.EnteredQuantity=1")
                 .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/28/1")
+                .post("/addproducttocart/details/28/1")
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -41,7 +63,7 @@ public class ShopTests {
                 .cookie("Nop.customer=21045fcb-7782-4d01-81a1-fda4f9b86b51")
                 .body("product_attribute_28_7_10=26&product_attribute_28_1_11=30&addtocart_28.EnteredQuantity=1")
                 .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/28/2")
+                .post("/addproducttocart/details/28/2")
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -58,19 +80,19 @@ public class ShopTests {
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                         .body("Email=alex_wild%40mail.ru&Password=123456&RememberMe=false")
                         .when()
-                        .post("http://demowebshop.tricentis.com/login")
+                        .post("/login")
                         .then()
                         .log().all()
                         .statusCode(302)
                         .extract()
                         .cookie("NOPCOMMERCE.AUTH");
 
-        open("http://demowebshop.tricentis.com/");
+        open(baseURI);
 
         getWebDriver().manage().addCookie(
                 new Cookie("NOPCOMMERCE.AUTH", authorizationCookie));
 
-        open("http://demowebshop.tricentis.com/customer/info");
+        open(baseURI + "/customer/info");
 
         $x("//label[@for='Email']//following-sibling::input").shouldHave(value("alex_wild@mail.ru"));
     }
@@ -80,14 +102,14 @@ public class ShopTests {
 
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("Nop.customer=21045fcb-7782-4d01-81a1-fda4f9b86b51")
+                .cookie("Nop.customer=efa04ead-b036-4630-86b1-5bd8875d03c6")
                 .when()
-                .get("http://demowebshop.tricentis.com/search?q=ring")
+                .get("/search?q=ring")
                 .then()
                 .log().all()
                 .statusCode(200);
 
-        open("http://demowebshop.tricentis.com/search?q=ring");
+        open(baseURI + "/search?q=ring");
 
         $x("//div[@class='product-grid']").shouldHave(text("Diamond Engagement Ring"));
 
